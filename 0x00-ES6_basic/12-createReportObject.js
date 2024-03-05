283f837ef3d055561b3b99e4606ead0f069b40e0
Alfonce Morara <alfoncemorara412@gmail.com>
2024-03-05 07:49:47 +0300

    Update 12-createReportObject.js
    finalcommit

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,17 +1,3 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  // Group employees by department using spread syntax and object destructuring
-  for (const { department, name } of employeesList) {
-    allEmployees[department] = allEmployees[department] || [];
-    allEmployees[department].push(name);
-  }
-
-  return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(allEmployees).length;
-    },
-  };
+  return { allEmployees: { ...employeesList } };
 }
-
