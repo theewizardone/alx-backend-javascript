e12d4692c0085052d70f8c66647995186a663d57
Alfonce Morara <alfoncemorara412@gmail.com>
2024-03-05 07:55:25 +0300

    Update 101-iterateThroughObject.js
    Edited file

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = [];
-  for (const employee of reportWithIterator) {
-    employeeNames.push(employee);
+  if (!reportWithIterator) {
+    console.log('Placeholder');
   }
-
-  return employeeNames.join('|');
 }
-
