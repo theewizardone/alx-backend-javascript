96f2450babef6279caa1f0cbe10443d732001464
Alfonce Morara <alfoncemorara412@gmail.com>
2024-03-06 23:51:46 +0300

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then(() => {
+  return promise.then(() => (
+    { status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      resolve({ status: 200, body: 'success' });
-    }).catch(() => {
-      console.log('Got an error response from the API');
-      reject(new Error());
-    }).finally(() => {
-      console.log('Got a response from the API');
-      resolve({ status: 200, body: 'success' });
     });
-  });
 }
